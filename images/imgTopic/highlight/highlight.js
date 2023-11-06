3699cca703de860299beda21fbc86b53a4ff3d3d
had191 <senna2k20@gmail.com>
2023-11-06 15:55:26 +0700

    Updates

---
+++ images/imgTopic/highlight/highlight.js
@@ -0,0 +1,20 @@
+export const Highlight = [
+  {
+    src: '/imgTopic/highlight/img-news.jpg',
+    title: 'BXH Nhạc Mới'
+  },
+  {
+    src: '/imgTopic/highlight/img-top100.jpg',
+    title: 'Top 100'
+  },
+  {
+    src: '/imgTopic/highlight/img-story.jpg',
+    title: 'Artist\'s Story'
+  },
+  {
+    src: '/imgTopic/highlight/img-chill.jpg',
+    title: 'Chill/Thư Giãn'
+  },
+
+]
+
