5344906a3e034837bc15675cae6573222f3e5ab3
had191 <senna2k20@gmail.com>
2024-05-28 22:58:43 +0700

    Change Package.json #2

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
